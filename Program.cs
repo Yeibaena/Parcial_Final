48bd68d8f77b2e3ceb31a470bdc3d9aa466de69b
YEISON RAMIREZ BAENA <yeibaena@gmailcom>
2024-11-29 20:20:01 -0500

    1.1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Tamaño predeterminado de la matriz
+        int rows = 5;
+        int cols = 5;
+
+
+    }
+}
+
+
+
