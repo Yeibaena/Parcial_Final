663b148b128f6d7bbd9af067b9a494211e8da38b
YEISON RAMIREZ BAENA <yeibaena@gmailcom>
2024-11-29 20:24:25 -0500

    1.2

--- Program.cs
+++ Program.cs
@@ -7,6 +7,19 @@ internal class Program
         int rows = 5;
         int cols = 5;
 
+        // Crear y llenar la matriz
+        int[,] matriz = new int[rows, cols];
+        LlenarMatrizConAleatorios(matriz);
+
+        // Variables para el recorrido y la suma
+        int sumaTotal = 0;
+        int posX = 0, posY = 0;
+
+        bool salir = false;
+        while (!salir)
+        {
+
+
 
     }
 }
